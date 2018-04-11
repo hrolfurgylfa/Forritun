b9084b3ea45b7feed46b4774ea53189976c6ddf5
hrolfurgylfa <hrolfurgylfa@gmail.com>
2018-04-11 16:24:09 +0000

    Tæk, Gönguljós taka 2

--- Tæk/KEST2VJ05BU/Arduino/Skóli/Gonguljos/Gonguljos.ino
+++ Tæk/KEST2VJ05BU/Arduino/Skóli/Gonguljos/Gonguljos.ino
@@ -1,8 +1,71 @@
-void setup() {
-  
+const int B_R_LED = 0;
+const int B_Gu_LED = 1;
+const int B_Gr_LED = 2;
 
+const int G_R_LED = 6;
+const int G_G_LED = 7;
+
+const int Btn_1 = 8;
+
+void setup(){
+  pinMode(0,OUTPUT);
+  pinMode(1,OUTPUT);
+  pinMode(2,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,INPUT);
+
+  digitalWrite(B_Gr_LED,HIGH);
+  digitalWrite(G_R_LED,HIGH);
 }
 
-void loop() {
-  
+void gongu_blikk(){
+  digitalWrite(G_G_LED,LOW);
+  digitalWrite(G_G_LED,HIGH);
+}
+
+void loop(){
+  if(digitalRead(Btn_1) == HIGH){
+
+    //Gult hja bilunum
+    digitalWrite(B_Gr_LED,LOW);
+    digitalWrite(B_Gu_LED,HIGH);
+    delay(1000);
+
+    //Rautt hja bilunum
+    digitalWrite(B_Gu_LED,LOW);
+    digitalWrite(B_R_LED,HIGH);
+    delay(1000);
+
+    //Grænt hjá gangandi
+    digitalWrite(G_R_LED,LOW);
+    digitalWrite(G_G_LED,HIGH);
+    delay(9000);
+
+    gongu_blikk();
+    delay(200);
+    gongu_blikk();
+    delay(200);
+    gongu_blikk();
+    delay(200);
+    gongu_blikk();
+    delay(200);
+    gongu_blikk();
+    delay(200);
+
+    //Rautt hjá gangandi
+    digitalWrite(G_G_LED,LOW);
+    digitalWrite(G_R_LED,HIGH);
+    delay(1000);
+
+    //Gult hjá bílum
+    digitalWrite(B_Gu_LED,HIGH);
+    delay(1000);
+
+    //Grænt hjá bílum
+    digitalWrite(B_Gu_LED,LOW);
+    digitalWrite(B_R_LED,LOW);
+    digitalWrite(B_Gr_LED,HIGH);
+    delay(10000);
+  }
 }
