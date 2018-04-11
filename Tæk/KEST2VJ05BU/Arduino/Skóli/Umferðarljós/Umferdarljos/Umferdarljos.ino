0b6652c841649e9f9d239085af817be8c02b27c5
hrolfurgylfa <hrolfurgylfa@gmail.com>
2018-04-11 16:14:55 +0000

    Tæk, Gönguljós

--- Tæk/KEST2VJ05BU/Arduino/Skóli/Umferðarljós/Umferdarljos/Umferdarljos.ino
+++ Tæk/KEST2VJ05BU/Arduino/Skóli/Umferðarljós/Umferdarljos/Umferdarljos.ino
@@ -1,56 +1,71 @@
-const int gr1_LED = 0;
-const int gu1_LED = 1;
-const int r1_LED = 2;
-const int gr2_LED = 3;
-const int gu2_LED = 4;
-const int r2_LED = 5;
+const int B_R_LED = 0;
+const int B_Gu_LED = 1;
+const int B_Gr_LED = 2;
+
+const int G_R_LED = 6;
+const int G_G_LED = 7;
+
+const int Btn_1 = 8;
 
 void setup(){
-    pinMode(0,OUTPUT);
-    pinMode(1,OUTPUT);
-    pinMode(2,OUTPUT);
-    pinMode(3,OUTPUT);
-    pinMode(4,OUTPUT);
-    pinMode(5,OUTPUT);
+  pinMode(0,OUTPUT);
+  pinMode(1,OUTPUT);
+  pinMode(2,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,INPUT);
 
-    digitalWrite(gu1_LED,LOW);
-    digitalWrite(r1_LED,HIGH);//Ljós 1 Rauð #1
-    delay(1000);
-    digitalWrite(gu2_LED,LOW);
-    digitalWrite(r2_LED,LOW);
-    digitalWrite(gr2_LED,HIGH);//Ljós 2 Græn #2
+  digitalWrite(B_Gr_LED,HIGH);
+  digitalWrite(G_R_LED,HIGH);
 }
+
+void gongu_blikk(){
+  digitalWrite(G_G_LED,LOW);
+  digitalWrite(G_G_LED,HIGH);
+}
+
 void loop(){
-    //Ljós 1 eru gul og ljós 2 eru rauð og gul
+  if(digitalRead(Btn_1) == HIGH){
+
+    //Gult hja bilunum
+    digitalWrite(B_Gr_LED,LOW);
+    digitalWrite(B_Gu_LED,HIGH);
+    delay(1000);
+
+    //Rautt hja bilunum
+    digitalWrite(B_Gu_LED,LOW);
+    digitalWrite(B_R_LED,HIGH);
+    delay(1000);
+
+    //Grænt hjá gangandi
+    digitalWrite(G_R_LED,LOW);
+    digitalWrite(G_G_LED,HIGH);
+    delay(9000);
+
+    gongu_blikk();
+    delay(200);
+    gongu_blikk();
+    delay(200);
+    gongu_blikk();
+    delay(200);
+    gongu_blikk();
+    delay(200);
+    gongu_blikk();
+    delay(200);
+
+    //Rautt hjá gangandi
+    digitalWrite(G_G_LED,LOW);
+    digitalWrite(G_R_LED,HIGH);
+    delay(1000);
+
+    //Gult hjá bílum
+    digitalWrite(B_Gu_LED,HIGH);
+    delay(1000);
+
+    //Grænt hjá bílum
+    digitalWrite(B_Gu_LED,LOW);
+    digitalWrite(B_R_LED,LOW);
+    digitalWrite(B_Gr_LED,HIGH);
     delay(10000);
-    
-    digitalWrite(gr2_LED,LOW);
-    digitalWrite(gu2_LED,HIGH);//Ljós 2 Gul #3
-    delay(1000);
-    
-    digitalWrite(gu2_LED,LOW);
-    digitalWrite(r2_LED,HIGH);//Ljós 2 Rauð #4
-    delay(1000);
-    digitalWrite(gu1_LED,HIGH);//Ljós 1 Rauð og Gul #5
-    delay(1000);
-    
-    digitalWrite(r1_LED,LOW);
-    digitalWrite(gu1_LED,LOW);
-    digitalWrite(gr1_LED,HIGH);//Ljós 1 Græn #6
-    delay(10000);
-    
-    digitalWrite(gr1_LED,LOW);
-    digitalWrite(gu1_LED,HIGH);//Ljós 1 Gul #7
-    delay(1000);
-    
-    digitalWrite(gu1_LED,LOW);
-    digitalWrite(r1_LED,HIGH);//Ljós 1 Rauð #8
-    delay(1000);
-    
-    digitalWrite(gu2_LED,HIGH);//Ljós 2 Rauð og Gul #9
-    delay(1000);
-    
-    digitalWrite(gu2_LED,LOW);
-    digitalWrite(r2_LED,LOW);
-    digitalWrite(gr2_LED,HIGH);//Ljós 2 Græn #10
+  }
 }
