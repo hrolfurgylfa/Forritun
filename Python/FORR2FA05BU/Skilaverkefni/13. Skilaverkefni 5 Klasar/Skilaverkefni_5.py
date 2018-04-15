a3d086e775e96a49109601636eab3d66f36ae680
hrolfurgylfa <hrolfurgylfa@gmail.com>
2018-04-15 21:15:31 +0000

    Python, Comment comin á verkefni 5

--- Python/FORR2FA05BU/Skilaverkefni/13. Skilaverkefni 5 Klasar/Skilaverkefni_5.py
+++ Python/FORR2FA05BU/Skilaverkefni/13. Skilaverkefni 5 Klasar/Skilaverkefni_5.py
@@ -1,34 +1,33 @@
 '''
------Verkefni-----
+Skilaverkefni 5
 Hrólfur Gylfason
------Dagsetning-----
 '''
 import math
 
-class Hringur():
-    def ummal(radius):
+class Hringur():#Þessi klasi reiknar ummál, flatarmál eða rúmmál hrings eftir því hvað er kallað á
+    def ummal(radius):#Þetta fall tekur inn radius, reiknar ummál hringsins og skilar svo ummálinu
         return radius * 2 * math.pi
-    def flatarmal(radius):
+    def flatarmal(radius):#Þetta fall tekur inn radius, reiknar flatarmál hringsins og skilar svo flatarmálinu
         return radius * radius * math.pi
-    def rummal(radius):
+    def rummal(radius):#Þetta fall tekur inn radius, reiknar rúmmál kúlunar og skilar svo rúmmálinu
         return (4 * math.pi * (radius * radius * radius)) / 3
 
-class Jofnur():
-    def jafna1(x):
+class Jofnur():#Þessi klasi reiknar tvær mismunandi jöfnur eftir því hvað er kallað á
+    def jafna1(x):#Þetta fall tekur inn gildið x og skilar svo svarinu hvað y er
         y = 3*x + 4 + 2*(x*x*x)
         return y
-    def jafna2(x,z):
+    def jafna2(x,z):#Þetta fall tekur inn gildið x og z og skilar svo svarinu hvað y er
         y = ((z*z)+(x*x))*4
         return y
 
-class Hnit():
-    def __init__(self,x,y):
+class Hnit():#Þessi klasi reiknar hnit
+    def __init__(self,x,y):#Þetta fall er til þess að búa til gildin self.x og self.y sem eru svo notuð seinna í klasanum
         self.x = x
         self.y = y
 
-    def hnitaPrentari(self):
+    def hnitaPrentari(self):#Þetta fall prentar bara gildi x og y
         print("x hnitið er",self.x,"og y hnitið er",self.y)
-    def hlutaPrentari(self):
+    def hlutaPrentari(self):#Þetta fall skilar á hvaða helmingi hnitið er, 1, 2, 3, 4 eða 0 ef hnitið er með 0 í annaðhvort x eða y svo að það sé ekki á neinum hluta
         if x > 0 and y > 0:
             return 2
         elif x < 0 and y > 0:
@@ -38,7 +37,7 @@ class Hnit():
         elif x > 0 and y < 0:
             return 4
         elif x == 0 or y == 0:
-            return "0"
+            return 0
 
 valmynd = ""
 
