f129f0c5247d752f6abf2e81bd33179fd4b089fd
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2018-10-31 09:46:56 +0000

    Liður 1 kominn

--- Python/FORR2HF05CU/Æfingarverkefni/14. Æfingarverkefni 7 recursion/Æfingarverkefni_7.py
+++ Python/FORR2HF05CU/Æfingarverkefni/14. Æfingarverkefni 7 recursion/Æfingarverkefni_7.py
@@ -3,9 +3,16 @@
 Hrólfur Gylfason
 31/10/2018
 '''
+def finnaHeildarsummu(tala, summa = 0):
+    if tala > 0:
+        summa += tala
+        return finnaHeildarsummu(tala-1, summa)
+    else:
+        return summa
+
 valmynd = ""
 
-while valmynd != "14":
+while valmynd != "3":
 
     for tel in range(50):#Þessi for lúppa gerir línu sem er auðvelt að stjórna stærðinni á
         print("-",end="")
@@ -13,18 +20,7 @@ while valmynd != "14":
 
     print("Ýttu á 1 til þess að fá dæmi 1")
     print("Ýttu á 2 til þess að fá dæmi 2")
-    print("Ýttu á 3 til þess að fá dæmi 3")
-    print("Ýttu á 4 til þess að fá dæmi 4")
-    print("Ýttu á 5 til þess að fá dæmi 5")
-    print("Ýttu á 6 til þess að fá dæmi 6")
-    print("Ýttu á 7 til þess að fá dæmi 7")
-    print("Ýttu á 8 til þess að fá dæmi 8")
-    print("Ýttu á 9 til þess að fá dæmi 9")
-    print("Ýttu á 10 til þess að fá dæmi 10")
-    print("Ýttu á 11 til þess að fá dæmi 11")
-    print("Ýttu á 12 til þess að fá dæmi 12")
-    print("Ýttu á 13 til þess að fá dæmi 13")
-    print("Ýttu á 14 til þess að hætta")
+    print("Ýttu á 3 til þess að hætta")
     valmynd = input("-------------------->>> ")#Hérna velur notandinn hvaða lið hann ætlar að fara í
 
     print()#Þetta er til þess að gera enter
@@ -33,46 +29,13 @@ while valmynd != "14":
     print()#Þetta er til þess að gera enter
 
     if valmynd == "1":#Liður 1
-        pass
+        print("Úr return:",finnaHeildarsummu(7))
 
     elif valmynd == "2":#Liður 2
         pass
-        
-    elif valmynd == "3":#Liður 3
-        pass
 
-    elif valmynd == "4":#Liður 4
-        pass
-        
-    elif valmynd == "5":#Liður 5
-        pass
-
-    elif valmynd == "6":#Liður 6
-        pass
-
-    elif valmynd == "7":#Liður 7
-        pass
-
-    elif valmynd == "8":#Liður 8
-        pass
-
-    elif valmynd == "9":#Liður 9
-        pass
-        
-    elif valmynd == "10":#Liður 10
-        pass
-
-    elif valmynd == "11":#Liður 11
-        pass
-        
-    elif valmynd == "12":#Liður 12
-        pass
-
-    elif valmynd == "13":#Liður 13
-        pass
-
-    elif valmynd == "14":#Þetta er til þess að það komi ekki "ERROR Sláðu inn tölu á milli 1 og 14" þegar maður er að hætta í forritinu
+    elif valmynd == "3":#Þetta er til þess að það komi ekki "ERROR Sláðu inn tölu á milli 1 og 3" þegar maður er að hætta í forritinu
         pass
 
     else:
-        print("ERROR\tSláðu inn tölu á milli 1 og 14")
+        print("ERROR\tSláðu inn tölu á milli 1 og 3")
