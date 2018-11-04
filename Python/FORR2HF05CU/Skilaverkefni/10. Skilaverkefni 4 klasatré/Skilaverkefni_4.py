0ec6c10f22b0d3ee65eeeab2ff50c784617b62bf
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2018-11-04 18:50:01 +0000

    Skilaverkefni 4 klárað og commentað

--- Python/FORR2HF05CU/Skilaverkefni/10. Skilaverkefni 4 klasatré/Skilaverkefni_4.py
+++ Python/FORR2HF05CU/Skilaverkefni/10. Skilaverkefni 4 klasatré/Skilaverkefni_4.py
@@ -8,31 +8,21 @@ from Skilaverkefni_4_klasar import *
 
 valmynd = ""
 
+#Hérna bý ég til öll tilvik klasana til þess að geta notað tilvikin í nokkrum mismunandi liðum
 tilvik1 = Nemi(2109013290, "Hrólfur", "kk", "Holtagerði 7", 6155549, "hrolfurgylfa@protonmail.com")
 tilvik2 = Grunnskolanemi(2109013290, "Hrólfur", "kk", "Holtagerði 7", 6155549, "hrolfurgylfa@protonmail.com", "Gylfi", "Kársnesskóli")
 tilvik3 = Framhaldsskolanemi(2109013290, "Hrólfur", "kk", "Holtagerði 7", 6155549, "hrolfurgylfa@protonmail.com", "Forritun")
 tilvik4 = Haskolanemi(2109013290, "Hrólfur", "kk", "Holtagerði 7", 6155549, "hrolfurgylfa@protonmail.com", 3, 1450000)
 
-while valmynd != "14":
+while valmynd != "3":
 
     for tel in range(50):#Þessi for lúppa gerir línu sem er auðvelt að stjórna stærðinni á
         print("-",end="")
     print("\n")#Þetta er til þess að gera tvö enter
 
-    print("Ýttu á 1 til þess að fá __str__")
-    print("Ýttu á 2 til þess að fá dæmi 2")
-    print("Ýttu á 3 til þess að fá dæmi 3")
-    print("Ýttu á 4 til þess að fá dæmi 4")
-    print("Ýttu á 5 til þess að fá dæmi 5")
-    print("Ýttu á 6 til þess að fá dæmi 6")
-    print("Ýttu á 7 til þess að fá dæmi 7")
-    print("Ýttu á 8 til þess að fá dæmi 8")
-    print("Ýttu á 9 til þess að fá dæmi 9")
-    print("Ýttu á 10 til þess að fá dæmi 10")
-    print("Ýttu á 11 til þess að fá dæmi 11")
-    print("Ýttu á 12 til þess að fá dæmi 12")
-    print("Ýttu á 13 til þess að fá dæmi 13")
-    print("Ýttu á 14 til þess að hætta")
+    print("Ýttu á 1 til þess að fá __str__ föll")
+    print("Ýttu á 2 til þess að fá get og set föll")
+    print("Ýttu á 3 til þess að hætta")
     valmynd = input("-------------------->>> ")#Hérna velur notandinn hvaða lið hann ætlar að fara í
 
     print()#Þetta er til þess að gera enter
@@ -40,60 +30,37 @@ while valmynd != "14":
         print("-",end="")
     print()#Þetta er til þess að gera enter
 
-    if valmynd == "1":#Liður 1
+    if valmynd == "1":#__str__ föll
         print("Nemi:")
         print(tilvik1)
-        print()
+        print()#Enter
 
         print("Grunnskólanemi:")
         print(tilvik2)
-        print()
+        print()#Enter
 
         print("Framhaldsskóli:")
         print(tilvik3)
-        print()
+        print()#Enter
 
         print("Háskóli:")
         print(tilvik4)
 
-    elif valmynd == "2":#Liður 2
-        pass
-        
-    elif valmynd == "3":#Liður 3
-        pass
+    elif valmynd == "2":#get og set föll
+        tilvik1.netfang_set("hrolfurgylfa@hrolfurgylfa.is")
+        print("Nemi netfang:",tilvik1.netfang_get())
 
-    elif valmynd == "4":#Liður 4
-        pass
-        
-    elif valmynd == "5":#Liður 5
-        pass
+        tilvik2.forradamadur_set("Helga")
+        print("Grunnskoli forráðamaður:",tilvik2.forradamadur_get())
 
-    elif valmynd == "6":#Liður 6
-        pass
+        tilvik3.brautarheiti_set("Nát")
+        print("Framhaldsskólanemi brautarheiti:",tilvik3.brautarheiti_get())
 
-    elif valmynd == "7":#Liður 7
-        pass
+        tilvik4.namslan_set("Nei")
+        print("Háskóli námslán:",tilvik4.namslan_get())
 
-    elif valmynd == "8":#Liður 8
-        pass
-
-    elif valmynd == "9":#Liður 9
-        pass
-        
-    elif valmynd == "10":#Liður 10
-        pass
-
-    elif valmynd == "11":#Liður 11
-        pass
-        
-    elif valmynd == "12":#Liður 12
-        pass
-
-    elif valmynd == "13":#Liður 13
-        pass
-
-    elif valmynd == "14":#Þetta er til þess að það komi ekki "ERROR Sláðu inn tölu á milli 1 og 14" þegar maður er að hætta í forritinu
+    elif valmynd == "3":#Þetta er til þess að það komi ekki "ERROR Sláðu inn tölu á milli 1 og 3" þegar maður er að hætta í forritinu
         pass
 
     else:
-        print("ERROR\tSláðu inn tölu á milli 1 og 14")
+        print("ERROR\tSláðu inn tölu á milli 1 og 3")
--- Python/FORR2HF05CU/Skilaverkefni/10. Skilaverkefni 4 klasatré/Skilaverkefni_4_klasar.py
+++ Python/FORR2HF05CU/Skilaverkefni/10. Skilaverkefni 4 klasatré/Skilaverkefni_4_klasar.py
@@ -7,29 +7,27 @@ class Nemi():
         self.simanumer = simanumer
         self.netfang = netfang
         
-    def __str__(self):
+    def __str__(self):#__str__ föll prentast út þegar maður reynir að prenta tilvik af klasanum
         return "Kennitala: "+str(self.kt)+"\nNafn: "+str(self.nafn)+"\nKyn: "+str(self.kyn)+"\nHeimilisfang: "+str(self.heimilisfang)+"\nSímanúmer: "+str(self.simanumer)+"\nNetfang: "+str(self.netfang)
 
-    def netfang_getter(self):
+    def netfang_get(self):#skilar self.netfang ef það er @ í netfanginu
         netfang = self.netfang
 
         rett_netfang = False
         for stafur in netfang:
             if stafur == "@":
-                if stafur == ".":
-                    rett_netfang = True
+                rett_netfang = True
         
         if rett_netfang is not True:
-            return ""
+            return "Rangt netfang"
 
         return netfang
 
-    def netfang_setter(self, netfang):
+    def netfang_set(self, netfang):#breytir self.netfang í netfang færibreytuna ef það er @ í færibreytunni netfang
         rett_netfang = False
         for stafur in netfang:
             if stafur == "@":
-                if stafur == ".":
-                    rett_netfang = True
+                rett_netfang = True
         
         if rett_netfang is not True:
             return False
@@ -38,20 +36,32 @@ class Nemi():
 
 class Grunnskolanemi(Nemi):
     def __init__(self, kt, nafn, kyn, heimilisfang, simanumer, netfang, forradamadur, nafnSkola):
-        Nemi.__init__(self, kt, nafn, kyn, heimilisfang, simanumer, netfang)
+        Nemi.__init__(self, kt, nafn, kyn, heimilisfang, simanumer, netfang)#Þetta initialize-ar allt úr klasanum Nemi
         self.forradamadur = forradamadur
         self.nafnSkola = nafnSkola
 
-    def __str__(self):
+    def forradamadur_get(self):#skilar self.forradamadur
+        return self.forradamadur
+    
+    def forradamadur_set(self, forradamadur):#breytir self.forradamadur í breytuna forradamadur sem kemur inn sem færibreyta
+        self.forradamadur = forradamadur
+
+    def __str__(self):#Svipað og sama fall í Nemi nema með fleiri hlutum í
         return "Kennitala: "+str(self.kt)+"\nNafn: "+str(self.nafn)+"\nKyn: "+str(self.kyn)+"\nHeimilisfang: "+str(self.heimilisfang)+"\nSímanúmer: "+str(self.simanumer)+"\nNetfang: "+str(self.netfang)+"\nForráðamaður: "+str(self.forradamadur)+"\nnafnSkóla: "+str(self.nafnSkola)
 
 class Framhaldsskolanemi(Nemi):
     def __init__(self, kt, nafn, kyn, heimilisfang, simanumer, netfang, brautarheiti, busetustyrkur = False):
-        Nemi.__init__(self, kt, nafn, kyn, heimilisfang, simanumer, netfang)
+        Nemi.__init__(self, kt, nafn, kyn, heimilisfang, simanumer, netfang)#Þetta initialize-ar allt úr klasanum Nemi
         self.brautarheiti = brautarheiti
         self.busetustyrkur = busetustyrkur
 
-    def __str__(self):
+    def brautarheiti_get(self):#skilar self.brautarheiti
+        return self.brautarheiti
+
+    def brautarheiti_set(self, brautarheiti):#breytir self.brautarheiti í færibreytuna brautarheiti
+        self.brautarheiti = brautarheiti
+
+    def __str__(self):#Þetta fall virkar eins og í klasanum nema þetta prentar út nei ef busetustyrkur er false en annars busetustyrkur og bætir við kr. fyrir aftan töluna
         if self.busetustyrkur is not False:
             return "Kennitala: "+str(self.kt)+"\nNafn: "+str(self.nafn)+"\nKyn: "+str(self.kyn)+"\nHeimilisfang: "+str(self.heimilisfang)+"\nSímanúmer: "+str(self.simanumer)+"\nNetfang: "+str(self.netfang)+"\nbrautarheiti: "+str(self.brautarheiti)+"\nbusetustyrkur: "+str(self.busetustyrkur)+"kr."
         else:
@@ -59,13 +69,24 @@ class Framhaldsskolanemi(Nemi):
 
 class Haskolanemi(Nemi):
     def __init__(self, kt, nafn, kyn, heimilisfang, simanumer, netfang, stigNams, namslan = False):
-        Nemi.__init__(self, kt, nafn, kyn, heimilisfang, simanumer, netfang)
+        Nemi.__init__(self, kt, nafn, kyn, heimilisfang, simanumer, netfang)#Þetta initialize-ar allt úr klasanum Nemi
         self.stigNams = stigNams
         self.namslan = namslan
 
-    def __str__(self):
+    def namslan_get(self):#skilar "Nei ef self.namslan er false en annars skilar þetta namslan sem heiltölu"
+        if self.namslan is False:
+            return "Nei"
+        else:
+            return int(namslan)
+
+    def namslan_set(self, namslan):#gerir self.namslan að false ef færibreytan er "Nei" en annars verður self.namslan að færibreytunni namslan
+        if namslan == "Nei":
+            self.namslan = False
+        else:
+            self.namslan = namslan
+
+    def __str__(self):#Þetta fall virkar eins og í klasanum nema þetta prentar út nei ef namslan er false en annars namslan og bætir við kr. fyrir aftan töluna
         if self.namslan is not False:
             return "Kennitala: "+str(self.kt)+"\nNafn: "+str(self.nafn)+"\nKyn: "+str(self.kyn)+"\nHeimilisfang: "+str(self.heimilisfang)+"\nSímanúmer: "+str(self.simanumer)+"\nNetfang: "+str(self.netfang)+"\nstig náms: "+str(self.stigNams)+"\nnámslán: "+str(self.namslan)+"kr."
         else:
             return "Kennitala: "+str(self.kt)+"\nNafn: "+str(self.nafn)+"\nKyn: "+str(self.kyn)+"\nHeimilisfang: "+str(self.heimilisfang)+"\nSímanúmer: "+str(self.simanumer)+"\nNetfang: "+str(self.netfang)+"\nstig náms: "+str(self.stigNams)+"\nnámslán: Nei"
-
