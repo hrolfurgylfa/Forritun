7ec124df71d754fc4ea5e024b250cf109b000bd0
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2018-11-21 21:03:20 +0000

    Liður 1-4 komnir

--- Python/FORR2HF05CU/Skilaverkefni/17. Skilaverkefni 6 SQL/Skilaverkefni_6.py
+++ Python/FORR2HF05CU/Skilaverkefni/17. Skilaverkefni 6 SQL/Skilaverkefni_6.py
@@ -3,79 +3,9 @@ Skilaverkefni 6 MySQL
 Hrólfur Gylfason
 16/11/2018
 '''
-import pymysql.cursors
-import sys
+from skraning import Skraning
 
-def executeSQL(kodi):
-    try:
-        connection = pymysql.connect(host='tsuts.tskoli.is',
-                                    user='2109013290',
-                                    password='mypassword',
-                                    db='2109013290_afangaskraning',
-                                    charset='utf8mb4',
-                                    cursorclass=pymysql.cursors.DictCursor)
-    except pymysql.err.OperationalError as error:
-        host = ""
-        for stafur in str(error)[42::]:
-            if stafur != "'":
-                host += stafur
-            else:
-                break
-
-        if str(error) == """(2003, "Can't connect to MySQL server on '"""+str(host)+"""' ([Errno -2] Name or service not known)")""" or str(error) == """(2003, "Can't connect to MySQL server on '"""+str(host)+"""' (timed out)")""":
-            print("Náði ekki í "+str(host)+"\nHætti...")
-            sys.exit()
-        else:
-            print(error)
-            print("Hætti...")
-            sys.exit()
-
-    cursor = connection.cursor()
-
-    try:
-        cursor.execute(kodi)
-    except pymysql.err.ProgrammingError as error:
-        sql_error_kodi = ""
-        skilabod = ""
-        for stafur in str(error)[1::]:
-            if stafur in "1234567890":
-                sql_error_kodi += stafur
-            else:
-                break
-        for stafur in str(error)[8::]:
-            if stafur != '"':
-                skilabod += stafur
-            else:
-                break
-
-        if sql_error_kodi == "1064":
-            fjoldi_komma_fundnar = 0
-            sql_error = ""
-            for stafur in str(error)[7::]:
-                if stafur != "'" and fjoldi_komma_fundnar == 0:
-                    pass
-                elif stafur != "'" and fjoldi_komma_fundnar == 1:
-                    sql_error += stafur
-                elif stafur == "'" and fjoldi_komma_fundnar == 0:
-                    fjoldi_komma_fundnar += 1
-                else:
-                    break
-
-            print("SQL Stafsetningarvilla")
-            print("SQL error kóði:",sql_error_kodi)
-            print('Það er stafsetningarvilla nálægt "'+str(sql_error)+'".')
-            print("Hætti...")
-        else:
-            print("SQL villa")
-            print("SQL error kóði:",sql_error_kodi)
-            print("SQL error skilaboð:",skilabod)
-            print("Hætti...")
-        sys.exit()
-
-    result = cursor.fetchall()
-    connection.close()
-    return result
-
+s = Skraning()
 valmynd = ""
 
 while valmynd != "14":
@@ -91,12 +21,6 @@ while valmynd != "14":
     print("Ýttu á 5 til þess að fá dæmi 5")
     print("Ýttu á 6 til þess að fá dæmi 6")
     print("Ýttu á 7 til þess að fá dæmi 7")
-    print("Ýttu á 8 til þess að fá dæmi 8")
-    print("Ýttu á 9 til þess að fá dæmi 9")
-    print("Ýttu á 10 til þess að fá dæmi 10")
-    print("Ýttu á 11 til þess að fá dæmi 11")
-    print("Ýttu á 12 til þess að fá dæmi 12")
-    print("Ýttu á 13 til þess að fá dæmi 13")
     print("Ýttu á 14 til þess að hætta")
     valmynd = input("-------------------->>> ")#Hérna velur notandinn hvaða lið hann ætlar að fara í
 
@@ -106,16 +30,28 @@ while valmynd != "14":
     print()#Þetta er til þess að gera enter
 
     if valmynd == "1":#Liður 1
-        print(executeSQL("SELECT * FROM namsked;"))
+        s.nyr_medlimur("Hrólfur")
 
     elif valmynd == "2":#Liður 2
-        pass
+        s.nyr_afangi("HRÓL1HF05AU")
         
     elif valmynd == "3":#Liður 3
-        pass
+        s.prenta("namskeid")
 
     elif valmynd == "4":#Liður 4
-        pass
+        afangar_nemanda = s.skradurIafanga("Konráð Guðmundsson")
+
+        tel = 0
+        for afangi in afangar_nemanda:
+            tel += 1
+            for a in afangi:
+                key = a
+            if tel <= len(afangar_nemanda) - 2:
+                print(afangi[key],end=", ")
+            elif tel == len(afangar_nemanda) - 1:
+                print(afangi[key],end=" og ")
+            else:
+                print(afangi[key])
         
     elif valmynd == "5":#Liður 5
         pass
@@ -125,25 +61,7 @@ while valmynd != "14":
 
     elif valmynd == "7":#Liður 7
         pass
-
-    elif valmynd == "8":#Liður 8
-        pass
-
-    elif valmynd == "9":#Liður 9
-        pass
         
-    elif valmynd == "10":#Liður 10
-        pass
-
-    elif valmynd == "11":#Liður 11
-        pass
-        
-    elif valmynd == "12":#Liður 12
-        pass
-
-    elif valmynd == "13":#Liður 13
-        pass
-
     elif valmynd == "14":#Þetta er til þess að það komi ekki "ERROR Sláðu inn tölu á milli 1 og 14" þegar maður er að hætta í forritinu
         pass
 
---
+++ Python/FORR2HF05CU/Skilaverkefni/17. Skilaverkefni 6 SQL/skraning.py
@@ -0,0 +1,124 @@
+import pymysql.cursors
+import sys
+
+class Skraning:
+    def executeSQL(self, kodi):
+        try:
+            connection = pymysql.connect(host='tsuts.tskoli.is',
+                                        user='2109013290',
+                                        password='mypassword',
+                                        db='2109013290_afangaskraning',
+                                        charset='utf8mb4',
+                                        cursorclass=pymysql.cursors.DictCursor,
+                                        autocommit=True)
+        except pymysql.err.OperationalError as error:
+            host = ""
+            for stafur in str(error)[42::]:
+                if stafur != "'":
+                    host += stafur
+                else:
+                    break
+
+            if str(error) == """(2003, "Can't connect to MySQL server on '"""+str(host)+"""' ([Errno -2] Name or service not known)")""" or str(error) == """(2003, "Can't connect to MySQL server on '"""+str(host)+"""' (timed out)")""":
+                print("Náði ekki í "+str(host)+"\nHætti...")
+                sys.exit()
+            else:
+                print(error)
+                print("Hætti...")
+                sys.exit()
+
+        cursor = connection.cursor()
+
+        try:
+            cursor.execute(kodi)
+        except pymysql.err.ProgrammingError as error:
+            sql_error_kodi = ""
+            skilabod = ""
+            for stafur in str(error)[1::]:
+                if stafur in "1234567890":
+                    sql_error_kodi += stafur
+                else:
+                    break
+            for stafur in str(error)[8::]:
+                if stafur != '"':
+                    skilabod += stafur
+                else:
+                    break
+
+            if sql_error_kodi == "1064":
+                fjoldi_komma_fundnar = 0
+                sql_error = ""
+                for stafur in str(error)[7::]:
+                    if stafur != "'" and fjoldi_komma_fundnar == 0:
+                        pass
+                    elif stafur != "'" and fjoldi_komma_fundnar == 1:
+                        sql_error += stafur
+                    elif stafur == "'" and fjoldi_komma_fundnar == 0:
+                        fjoldi_komma_fundnar += 1
+                    else:
+                        break
+
+                print("SQL Stafsetningarvilla")
+                print("SQL error kóði:",sql_error_kodi)
+                print('Það er stafsetningarvilla nálægt "'+str(sql_error)+'".')
+                print("Hætti...")
+            else:
+                print("SQL villa")
+                print("SQL error kóði:",sql_error_kodi)
+                print("SQL error skilaboð:",skilabod)
+                print("Hætti...")
+            sys.exit()
+
+        result = cursor.fetchall()
+        connection.close()
+        return result
+
+    def nyr_medlimur(self,nafn):
+        s = Skraning()
+        s.executeSQL("INSERT INTO notendur(nafn) VALUES ('"+str(nafn)+"')")
+
+    def nyr_afangi(self,afangi):
+        s = Skraning()
+        s.executeSQL("INSERT INTO namskeid(afangaheiti) VALUES ('"+str(afangi)+"')")
+
+    def prenta(self,nafntoflu):
+        s = Skraning()
+        tafla = s.executeSQL("SELECT * FROM "+str(nafntoflu))
+
+        try:
+            lyklar = list(tafla[0].keys())
+        except IndexError:
+            return ""
+
+        for lykill in lyklar:
+            print(lykill,end="\t|\t")
+        print()
+
+        for rod in tafla:
+            for hlutur in rod:
+                print(rod[hlutur],end="\t|\t")
+            print()
+
+    def skradurIafanga(self,nafn):
+        s = Skraning()
+        return s.executeSQL("""
+        SELECT afangaheiti 
+        FROM namskeid 
+            JOIN skradir
+                ON namskeid.namskeid_id = skradir.namskeid_id
+            JOIN notendur
+                ON notendur.notandi_id = skradir.notandi_id
+        WHERE notendur.nafn='"""+str(nafn)+"""'
+        """)
+
+    def skraning(self,nafn,afangi):
+        s = Skraning()
+        try:
+            sql="select notandi_id from notendur where nafn='"+nafn+"'"
+            sql="select namskeid_id from namskeid where afangaheiti='"+afangi+"'"
+            sql="insert into skradir(notandi_id,namskeid_id) VALUES("+str(notandi_id)+","+str(namskeid_id)+")"
+
+        except IndexError as e:
+            print("Viðkomandi ekki skráður notandi eða áfangaheiti rangt\n",e)
+
+
