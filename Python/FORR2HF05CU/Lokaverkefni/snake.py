9aff8de1888b2f859a71731d13dfe6925a776eea
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2018-11-28 09:27:52 +0000

    fall komið til þess að byrja pygame og gluggi gerður hvítur

--- Python/FORR2HF05CU/Lokaverkefni/snake.py
+++ Python/FORR2HF05CU/Lokaverkefni/snake.py
@@ -5,17 +5,25 @@ Hrólfur Gylfason
 '''
 import pygame
 
-pygame.init()# Byrjar pygame
-
 # Litir
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
+def byrjaPygame(glugga_stærð):
+    pygame.init()# Byrjar pygame
+
+    if glugga_stærð != "f":
+        window = pygame.display.set_mode(glugga_stærð)# Býr til gluggann
+    else:
+        window = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    pygame.display.set_caption('Snake')# Setir titil á gluggann
+    window.fill(WHITE)
+
+    return pygame
 
 window_size = 640, 480# Stærð glugga
-window = pygame.display.set_mode(window_size)# Býr til gluggann
-fullscreen = False# Segir til um hvort leikurinn sé í fullscreen
-pygame.display.set_caption('Snake')# Setir titil á gluggann
+byrjaPygame(window_size)
+fullscreen = False
 
 running = True
 
@@ -28,16 +36,14 @@ while running:
         if event.type == pygame.KEYDOWN and event.key == pygame.K_f:# Ef ýtt er á F til þess að skipta a milli fullscreen og ekki fullscreen
             if fullscreen is False:
                 pygame.display.quit()
-                pygame.display.init()
-                screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+                pygame = byrjaPygame("f")
                 fullscreen = True
-
             elif fullscreen is True:
                 pygame.display.quit()
-                pygame.display.init()
-                screen = pygame.display.set_mode(window_size)
+                pygame = byrjaPygame(window_size)
                 fullscreen = False
 
         pygame.display.update()
+
 # Slekkur á pygame
 pygame.quit()
