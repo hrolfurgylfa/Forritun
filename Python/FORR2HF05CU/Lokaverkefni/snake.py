9e9145591320d18439f2b9525f03f8a93b4fcf97
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2018-11-28 09:48:25 +0000

    snakur sem fer afram kominn í leikinn

--- Python/FORR2HF05CU/Lokaverkefni/snake.py
+++ Python/FORR2HF05CU/Lokaverkefni/snake.py
@@ -8,6 +8,7 @@ import pygame
 # Litir
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+RED = (255, 0, 0)
 
 def byrjaPygame(glugga_stærð):
     pygame.init()# Byrjar pygame
@@ -17,13 +18,20 @@ def byrjaPygame(glugga_stærð):
     else:
         window = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
     pygame.display.set_caption('Snake')# Setir titil á gluggann
-    window.fill(WHITE)
 
     return (pygame, window)
 
 window_size = 640, 480# Stærð glugga
-byrjaPygame(window_size)
+pygame_tuple = byrjaPygame(window_size)
+pygame = pygame_tuple[0]
+window = pygame_tuple[1]
 fullscreen = False
+
+snakur_x = 0
+snakur_y = 0
+
+clock = pygame.time.Clock()
+clock_ticks = 20
 
 running = True
 
@@ -47,7 +55,13 @@ while running:
                 window = pygame_tuple[1]
                 fullscreen = False
 
-        pygame.display.update()
+    pygame.draw.rect(window, RED, pygame.Rect(snakur_x, 30, 20, 10))
+    pygame.display.update()
+    window.fill(WHITE)
+
+    snakur_x += 1
+
+    clock.tick(clock_ticks)
 
 # Slekkur á pygame
 pygame.quit()
