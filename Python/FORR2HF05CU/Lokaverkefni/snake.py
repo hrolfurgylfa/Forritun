243d71b758c5eb252875c5eb75ea9c9c2be78c34
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2018-12-06 16:43:15 +0000

    auka debug dút tekið út

--- Python/FORR2HF05CU/Lokaverkefni/snake.py
+++ Python/FORR2HF05CU/Lokaverkefni/snake.py
@@ -74,21 +74,6 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:# Ef ýtt er á Esc
             running = False
-        # if event.type == pygame.KEYDOWN and event.key == pygame.K_f:# Ef ýtt er á F til þess að skipta a milli fullscreen og ekki fullscreen
-        #     if fullscreen is False:
-        #         pygame.display.quit()
-        #         pygame_tuple = byrjaPygame("f")
-        #         pygame = pygame_tuple[0]
-        #         window = pygame_tuple[1]
-        #         fullscreen = True
-        #         fullscreen_w, fullscreen_h = pygame.display.get_surface().get_size()
-
-        #     elif fullscreen is True:
-        #         pygame.display.quit()
-        #         pygame_tuple = byrjaPygame(window_size)
-        #         pygame = pygame_tuple[0]
-        #         window = pygame_tuple[1]
-        #         fullscreen = False
 
         # Hreyfa snákinn
         if event.type == pygame.KEYDOWN and event.key == pygame.K_w or event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
@@ -111,20 +96,6 @@ while running:
                 velocity_x = 1
                 velocity_y = 0
                 vegalengd_fra_begju = 0
-
-        # Debug
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_1:
-            hradi = .1
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_2:
-            hradi = 2
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_3:
-            hradi = 5
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_z:
-            meiri_lengd = True
-        try:
-            x_mouse, y_mouse = event.pos
-        except AttributeError:
-            x_mouse, y_mouse = -500, -500
 
     # Bæta við mat fyrir snákinn ef hann er búinn að borða síðasta mat
     if matur_a_bordi is False:
@@ -149,11 +120,6 @@ while running:
             meiri_lengd = True
             stig += 1
     
-    # Debug
-    if x_mouse < matur_x + snakur_r*2 and x_mouse > matur_x - snakur_r*2 and y_mouse < matur_y + snakur_r*2 and y_mouse > matur_y - snakur_r*2:
-        matur_a_bordi = False
-        print("Namm")
-
     # Tékka hvort að snákurinn sé búinn að klessa á vegg
     if snakur_x[-1] < 0 + snakur_w or snakur_x[-1] > width - snakur_w or snakur_y[-1] < 0 + snakur_h or snakur_y[-1] > height - snakur_h:
         running = False
