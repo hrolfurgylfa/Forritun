f32b115319027c22e0d026882c48ff589db6c9cc
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2019-01-24 16:56:50 +0000

    verkenfi 2 byrjað, eitt tvö komin og 3 byrjað

---
+++ JS/FORR3JS05DU/Verkefni/Verkefni 2/kodi.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let manneskja = {
+    nafn: "Jón Jónsson",
+    kennitala: 2505004790,
+    heimilisfang: " 1",
+    heimasími: 5550357,
+    farsími: 7385023
+};
+
+for (let eiginleiki in manneskja) {
+    console.log(eiginleiki + ": " + manneskja[eiginleiki])
+}
+
+let manneskja = {
+    nafn: "Jón Jónsson",
+    kennitala: 2505004790,
+    heimilisfang: " 1",
+    heimasími: 5550357,
+    farsími: 7385023,
+    function nafnOgAldur () {
+        
+    }
+};
+
---
+++ JS/FORR3JS05DU/Verkefni/Verkefni 2/verkefni_2.md
@@ -0,0 +1,17 @@
+# Verkefni 2
+
+1.  ```javascript
+    let manneskja = {
+        nafn: "Jón Jónsson",
+        kennitala: 2505004790,
+        heimilisfang: " 1",
+        heimasími: 5550357,
+        farsími: 7385023
+    };
+    ```
+2.  ```javascript
+    for (let eiginleiki in manneskja) {
+        console.log(eiginleiki + ": " + manneskja[eiginleiki])
+    }
+
+
