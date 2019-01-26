0ed993db44fe6992b4d4230869bbfe8500e596ec
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2019-01-26 14:34:07 +0000

    dæmi 3 komið

--- JS/FORR3JS05DU/Verkefni/Verkefni 2/kodi.js
+++ JS/FORR3JS05DU/Verkefni/Verkefni 2/kodi.js
@@ -12,14 +12,18 @@ for (let eiginleiki in manneskja) {
     console.log(eiginleiki + ": " + manneskja[eiginleiki])
 }
 
-let manneskja = {
+let manneskja2 = {
     nafn: "Jón Jónsson",
+    aldur: 18,// Það var enginn aldur í lið 1 svo að ég bætti honum bara við hérna svo að ég gæti kallað í hann
     kennitala: 2505004790,
     heimilisfang: " 1",
     heimasími: 5550357,
     farsími: 7385023,
-    function nafnOgAldur () {
-        
+    nafnOgAldur : function() {
+        console.log(this.nafn+",");
+        console.log(this.aldur);
+        return;
     }
 };
 
+manneskja2.nafnOgAldur()
--- JS/FORR3JS05DU/Verkefni/Verkefni 2/verkefni_2.md
+++ JS/FORR3JS05DU/Verkefni/Verkefni 2/verkefni_2.md
@@ -13,5 +13,19 @@
     for (let eiginleiki in manneskja) {
         console.log(eiginleiki + ": " + manneskja[eiginleiki])
     }
-
+    ```
+3.  ```javascript
+    let manneskja = {
+        nafn: "Jón Jónsson",
+        aldur: 18,// Það var enginn aldur í lið 1 svo að ég bætti honum bara við hérna svo að ég gæti kallað í hann
+        kennitala: 2505004790,
+        heimilisfang: " 1",
+        heimasími: 5550357,
+        farsími: 7385023,
+        nafnOgAldur: function() {
+            console.log(this.nafn);
+            console.log(this.aldur);
+        }
+    };
+    ```
 
