9aec271f6f397876db860fc844c7c00d51c876e5
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2019-03-03 23:37:24 +0000

    leikur tilbúinn

--- docs/FORR3JS05DU/Geimbardagi/main.js
+++ docs/FORR3JS05DU/Geimbardagi/main.js
@@ -12,13 +12,29 @@ let FaRandomTolu = (min, max) => {
 }
 
 // ----- Breytur -----
-let staerdSpilara = 20;
+// Stillinga breytur
+let staerdSpilaraStilling = 20;
+let skotaHradiStilling = 2;
+let ovinaSpawnHradiStilling = 500;
+let lifStilling = 3;
+
+// Stillinga breytur gerdar
+let staerdSpilara = staerdSpilaraStilling;
+let skotaHradi = skotaHradiStilling;
+let ovinaSpawnHradi = ovinaSpawnHradiStilling;
+let lif = lifStilling;
+
+// Auto breytur
+let ovinaSpawnTeljari = ovinaSpawnHradi;
+let spilaraHitbox = staerdSpilara/1.5;
+let adalLoopID;
+let ovinirSkjotaID;
 let ollSkot = [];
 let allirOvinir = [];
-let ollOvinaSkot = [];
-let skotaHradi = 2;
-let ovinaSpawnHradi = 1000;
-let ovinaSpawnTeljari = 1000;
+let buid = false;
+let ovinirDrepnir = 0;
+let lifaTexti = document.getElementById("lifaTexti");// Þetta fær elementinn fyrir lífa teljarann svo að það þurfi ekki að gera það oft 
+let ovinirDrepnirTexti = document.getElementById("ovinirDrepnirTexti");// Þetta fær elementinn fyrir óvinir drepnir teljarann svo að það þurfi ekki að gera það oft
 
 // Gera takka tilbúinn
 let byrjaTakki = document.getElementById("takki");
@@ -27,19 +43,18 @@ let byrjaTakki = document.getElementById("takki");
 let canvas = document.getElementById("leikur");
 let ctx = canvas.getContext('2d');
 
-// ----- Klasar -----
-class Spilari {
-    constructor() {
-        this.x = 0;
-        this.y = 0;
-    }
+// ----- Klasar og Objects -----
+let Spilari = {
+    x: 0,
+    y: 0
 }
 class Skot {
-    constructor(x, y, att, litur) {
+    constructor(x, y, att, litur, origin) {
         this.x = x;
         this.y = y;
         this.att = att;
-        this.litur = litur
+        this.litur = litur;
+        this.originOvinur = origin;
     }
 }
 class Ovinir {
@@ -53,12 +68,12 @@ class Ovinir {
 // Physics föll
 let faeraSkot = () => {
     let i = 0;
-    while (true) {
+    while (ollSkot.length != i) {
         switch (ollSkot[i].att) {
             case "n":
                 if (ollSkot[i].y < -35) {// Eyða skoti ef það er farið út af skjánum
                     ollSkot.splice(i, 1);
-                    i--;
+                    i--;// Þetta er til þess að þegar ég eyði eynum hlut þá fari þetta einn til baka svo að ég hoppi ekki yfir einn hlut í listanum
                 } else {
                     ollSkot[i].y -= skotaHradi;// Færa skotið
                 }
@@ -88,9 +103,6 @@ let faeraSkot = () => {
                 }
                 break;
         }
-        if (ollSkot.length - 1 === i) {// Þetta lokar lúpunni ef listinn er búinn
-            break;
-        }
         i++;
     }
 }
@@ -114,9 +126,53 @@ let buaTilOvin = () => {
 let ovinirSkjota = () => {
     allirOvinir.forEach(ovinur => {
         ["n","s","a","v"].forEach(att => {
-            ollSkot.push(new Skot(ovinur.x, ovinur.y, att, "blue"));
+            ollSkot.push(new Skot(ovinur.x, ovinur.y, att, "blue", true));
         }); 
     });
+}
+let reset = () => {// Þetta fall endurræsir allar nauðsinlegar breytur til þess að það sé hægt að spila aftur
+    staerdSpilara = staerdSpilaraStilling;
+    skotaHradi = skotaHradiStilling;
+    ovinaSpawnHradi = ovinaSpawnHradiStilling;
+    lif = lifStilling;
+    buid = false;
+    ovinirDrepnir = 0;
+    allirOvinir = [];
+    ollSkot = [];
+}
+
+// Collition Detection
+let collitionDetection = () => {
+    if (ollSkot.length != 0) {
+        let i = 0;
+        while (ollSkot.length != i) {// Þessi lúppa keyrir þangað til listinn er búinn
+            if (ollSkot[i].originOvinur === true) {// Þetta gerist ef skotið sem er verið að fara í gegnum er frá óvini
+                if (Spilari.x < ollSkot[i].x + spilaraHitbox && Spilari.x > ollSkot[i].x - spilaraHitbox && Spilari.y < ollSkot[i].y + spilaraHitbox && Spilari.y > ollSkot[i].y - spilaraHitbox) {
+                    lif--;
+                    ollSkot.splice(i, 1);
+                    i--;// Þetta er til þess að þegar ég eyði eynum hlut þá fari þetta einn til baka svo að ég hoppi ekki yfir einn hlut í listanum
+                    lifaTexti.textContent = lif;// Þetta lækkar lífa counterinn
+                    if (lif === 0) {
+                        buid = true;
+                    }
+                }
+            } else if (allirOvinir.length != 0) {// Þetta gerist bara ef það eru skot frá spilaranum á vellinum og það er allavegana einn óvinur á vellinum
+                let i2 = 0;// Hérna bý ég til annan teljara til þess að fara í gegnum alla óvinina og sjá hvort að þeir séu búnir að rekast í skot spilarans
+                while (allirOvinir.length != i2) {// Þessi lúppa keyrir þangað til listinn er búinn
+                    if (allirOvinir[i2].x < ollSkot[i].x + 10 && allirOvinir[i2].x > ollSkot[i].x - 10 && allirOvinir[i2].y < ollSkot[i].y + 10 && allirOvinir[i2].y > ollSkot[i].y - 10) {
+                        // Hérna hefur óvinur verið drepinn
+                        allirOvinir.splice(i2, 1);
+                        i2--;// Þetta er til þess að þegar ég eyði eynum hlut þá fari þetta einn til baka svo að ég hoppi ekki yfir einn hlut í listanum
+                        // Hérna bæti ég óvininum við stigin
+                        ovinirDrepnir++;
+                        ovinirDrepnirTexti.textContent = ovinirDrepnir;
+                    }
+                    i2++;// Þetta hækkar teljarann um 1 til þess að fara í næsta hlut í listanum
+                }
+            }
+            i++;// Þetta hækkar teljarann um 1 til þess að fara í næsta hlut í listanum
+        }
+    }
 }
 
 // Teikni föll
@@ -130,11 +186,11 @@ let teiknaSpilara = (x, y) => {
 }
 let teiknaSkot = () => {
     ctx.lineWidth = 10;
-    ollSkot.forEach((skot) => {
+    ollSkot.forEach((skot) => {// Þetta gerist fyrir hvert skot
         ctx.strokeStyle = skot.litur;
         ctx.beginPath();
         ctx.moveTo(skot.x, skot.y)
-        switch (skot.att) {
+        switch (skot.att) {// Þetta tékkar á áttinni á skotinu og teiknar það í rétta átt
             case "n":
                 ctx.lineTo(skot.x, skot.y + 30);
                 break;
@@ -153,7 +209,7 @@ let teiknaSkot = () => {
 }
 let teiknaOvini = () => {
     ctx.fillStyle = "green";
-    allirOvinir.forEach(ovinur => {
+    allirOvinir.forEach(ovinur => {// Þetta gerist fyrir hvern óvin
         ctx.beginPath();
         ctx.arc(ovinur.x, ovinur.y, 20, 0, Math.PI*2, false);
         ctx.fill();
@@ -174,12 +230,29 @@ let adalLoop = () => {
     if (allirOvinir.length > 0) {ovinaStjorn();}// Færa alla óvini ef það eru eitthverjir óvinir á skjánum
 
     // Collition detection
+    collitionDetection();
 
     //Teikna
     ctx.clearRect(0, 0, canvas.width, canvas.height);// Hreinsa
     teiknaSkot();// Teikna öll skot spilara
     teiknaSpilara(Spilari.x, Spilari.y);// Teikna spilara
-    teiknaOvini();
+    teiknaOvini();// Teikna óvini
+
+    // Tékka hvort að leikurinn sé búinn
+    if (buid === true) {
+        clearInterval(adalLoopID);// Þetta stoppar setInterval sem kom í byrjun
+        clearInterval(ovinirSkjotaID);// Þetta stoppar setInterval sem kom í byrjun
+        ctx.clearRect(0, 0, canvas.width, canvas.height);// Hreinsa
+        byrjaTakki.classList.remove("fela");
+        byrjaTakki.textContent = "Spila aftur";
+        canvas.classList.remove("fela_mus");
+
+        // Skrifa buid texta
+        ctx.font = "80px Arial";
+        ctx.fillStyle = "red";
+        ctx.textAlign = "center";
+        ctx.fillText("Þú tapaðir", canvas.width/2, canvas.height/2); 
+    }
 }
 
 // ----- Uppákomu hlustarar -----
@@ -191,13 +264,16 @@ canvas.addEventListener("mousemove", (evt) => {
 });
 // Búa til skot þegar spilarinn ýtir á músina
 canvas.addEventListener("click", () => {
-    ollSkot.push(new Skot(Spilari.x, Spilari.y - 15, "n", "red"));
+    ollSkot.push(new Skot(Spilari.x, Spilari.y - 15, "n", "red", false));
 });
 
 // ----- Byrjun -----
-//byrjaTakki.addEventListener("click",function() {
+byrjaTakki.addEventListener("click",function() {
     if (canvas.getContext) {
-        setInterval(adalLoop,10);
-        setInterval(ovinirSkjota,1000);
+        reset();
+        byrjaTakki.classList.add("fela");
+        canvas.classList.add("fela_mus");
+        adalLoopID = setInterval(adalLoop,10);
+        ovinirSkjotaID = setInterval(ovinirSkjota,1000);
     }
-//});
+});
