574dd03b8b704bd6e2c6f12a142b2997cdf9a617
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2019-03-20 12:59:46 +0000

    forritunarkeppnis tími

---
+++ Python/FORK2FE02AU/Æfingar/Delta 2015/Demantar að Eilífu.py
@@ -0,0 +1,20 @@
+inntak = input()
+
+inntak = inntak.split()
+fjoldi = int(inntak[0])
+stafur = inntak[1]
+
+v = fjoldi
+h = fjoldi
+
+for x in range(1, fjoldi + 1):
+    if x == v and x == h: print(stafur,end="\n")
+    elif x == v: print(stafur,end="")
+    elif x == h - 1: print(stafur,end="\n")
+    else: print(" ",end="")
+    v -= 1
+    h += 1
+    print("v:",v)
+    print("h:",h)
+
+print()
