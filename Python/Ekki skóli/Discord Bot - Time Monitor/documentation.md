9a2c2732e1ec649b67d88ba5e257abed8a7b6707
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2019-04-29 13:23:16 +0000

    time monitor byrjaður (með nýtt repo nuna)

---
+++ Python/Ekki skóli/Discord Bot - Time Monitor/documentation.md
@@ -0,0 +1,3 @@
+# LPD User Monitor
+### Link to add the bot to a discord server
+https://discordapp.com/api/oauth2/authorize?client_id=565545302858268672&permissions=3072&scope=bot
--- Python/Ekki skóli/Discord Bot - Time Monitor/time_monitor_2.py
+++ Python/Ekki skóli/Discord Bot - Time Monitor/time_monitor_2.py
@@ -1,6 +1,10 @@
 import discord
 from discord.ext import commands
 
+admin_channel = "admin-bot-channel"
+bot_prefix = "?"
+
+
 token_file_name = "token.txt"
 def saekjaToken():
     token_file = open(token_file_name, "r")
@@ -8,9 +12,11 @@ def saekjaToken():
     token_file.close()
     return token
 
-client = discord.Client()
+async def sendErrorMessage(message, text):
+    await message.channel.send(message.author.mention+" "+str(text))
 
-allar_channels = client.get_all_channels()
+
+client = discord.Client()
 
 # @client.event
 # async def on_ready():
@@ -18,8 +24,28 @@ allar_channels = client.get_all_channels()
 
 @client.event
 async def on_message(message):
-    if message.content.find("?who") != -1:
-        await message.channel.send("This is Mr. Bot")
+    if message.content.find(bot_prefix+"who") != -1 and message.channel.name == admin_channel:
+
+        try:
+            argument = message.content[5::]
+        except IndexError:
+            await sendErrorMessage(message, "There is a missing argument. Do "+bot_prefix+"help for help")
+
+        channel_enumerator = client.get_all_channels()
+        channel_found = False
+        # channel = False
+        for channel in channel_enumerator:
+            if channel.name == argument:
+                channel_found = True
+                break
+        
+        if channel_found is True:
+            everyone_in_channel = ""
+            for member in channel.members:
+                everyone_in_channel = everyone_in_channel + "\n" + member.name
+            await message.channel.send("Here is everyone in the channel "+channel.name+":"+everyone_in_channel)
+
+        else: await sendErrorMessage(message, "This channel does not exist.")
         
 
 token = saekjaToken()
