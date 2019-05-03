6c9ed7ce1d9c1b23685304cb69561180fe69625b
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2019-05-03 09:41:40 +0000

    dagsetningar leit virkar

--- docs/FORR3JS05DU/Tonleikar/main.js
+++ docs/FORR3JS05DU/Tonleikar/main.js
@@ -21,16 +21,16 @@ let eftirJSONSótt = AllirTonleikar => {
 
     searchBtn.addEventListener("click", evt => search(evt.target.previousElementSibling.value.toLowerCase(), AllirTonleikar), false);
     searchBtn.previousElementSibling.addEventListener("change", evt => search(evt.target.value.toLowerCase(), AllirTonleikar), false);
-    eftirDagsetningu.addEventListener("change", tímaLeit, false);
-    fyrirDagsetningu.addEventListener("change", tímaLeit, false);
+    eftirDagsetningu.addEventListener("change", evt => tímaLeit(evt, AllirTonleikar), false);
+    fyrirDagsetningu.addEventListener("change", evt => tímaLeit(evt, AllirTonleikar), false);
 }
 let geraTonleika = tonleikar => {
     // Búa til myndacontainerinn
-    let myndacontainer = document.createElement("div");
+    let myndacontainer = buaTilElement("div");
     myndacontainer.classList.add("imageContainer");
     
     // Búa til myndina
-    let mynd = document.createElement("img");
+    let mynd = buaTilElement("img");
     mynd.src = tonleikar.imageSource;
     mynd.alt = "Mynd af tónleikunum "+tonleikar.eventDateName;
     mynd.classList.add("mynd");
@@ -71,28 +71,60 @@ let buaTilElement = (element, texti = false, foreldri = false, classList = false
 let search = (searchString, AllirTonleikar) => {
     eyðaÖllumBörnum(geymslaAllraTonleikaMynda);
 
+    // Hreinsa dagsetningar vegna þess að þær virka ekki með leitinni
+    eftirDagsetningu.value = "";
+    fyrirDagsetningu.value = "";
+
     AllirTonleikar.forEach(tonleikar => {
         if (tonleikar.eventDateName.toLowerCase().includes(searchString) || tonleikar.name.toLowerCase().includes(searchString) || tonleikar.userGroupName.toLowerCase().includes(searchString) || tonleikar.eventHallName.toLowerCase().includes(searchString)) {
             geraTonleika(tonleikar);
         }
     });
 }
-let tímaLeit = evt => {
+let tímaLeit = (evt, AllirTonleikar) => {
     eyðaÖllumBörnum(geymslaAllraTonleikaMynda);
+
+    // Hreinsa leitarslá vegna þess að hún virkar ekki með dagsetningunni
+    searchBtn.previousElementSibling.value = "";
 
     console.log("Eftir: "+eftirDagsetningu.value+"\nFyrir: "+fyrirDagsetningu.value);
 
     if (eftirDagsetningu.value != "" && fyrirDagsetningu.value != "") {
         // Finna tónleika á milli dagsetninga
-        console.log("Milli dagsetninga");
+        let eftirDagsetninguDate = new Date(eftirDagsetningu.value.slice(0,4), eftirDagsetningu.value.slice(5,7), eftirDagsetningu.value.slice(8,10));
+        let fyrirDagsetninguDate = new Date(fyrirDagsetningu.value.slice(0,4), fyrirDagsetningu.value.slice(5,7), fyrirDagsetningu.value.slice(8,10));
 
+        AllirTonleikar.forEach(tonleikar => {
+            let tonleikaDagsetning = new Date(tonleikar.dateOfShow.slice(0,4), tonleikar.dateOfShow.slice(5,7), tonleikar.dateOfShow.slice(8,10));
+            if (tonleikaDagsetning >= eftirDagsetninguDate && tonleikaDagsetning <= fyrirDagsetninguDate) {
+                geraTonleika(tonleikar);
+            }
+        });
     } else if (eftirDagsetningu.value != "" && fyrirDagsetningu.value === "") {
         // Finna tónleika eftir dagsetningu
-        console.log("Eftir dagsetningu");
+        let eftirDagsetninguDate = new Date(eftirDagsetningu.value.slice(0,4), eftirDagsetningu.value.slice(5,7), eftirDagsetningu.value.slice(8,10));
 
+        AllirTonleikar.forEach(tonleikar => {
+            let tonleikaDagsetning = new Date(tonleikar.dateOfShow.slice(0,4), tonleikar.dateOfShow.slice(5,7), tonleikar.dateOfShow.slice(8,10));
+            if (tonleikaDagsetning >= eftirDagsetninguDate) {
+                geraTonleika(tonleikar);
+            }
+        });
     } else if (eftirDagsetningu.value === "" && fyrirDagsetningu.value != "") {
         // Finna tónleika fyrir dagsetningu
-        console.log("Fyrir dagsetningu");
+        let fyrirDagsetninguDate = new Date(fyrirDagsetningu.value.slice(0,4), fyrirDagsetningu.value.slice(5,7), fyrirDagsetningu.value.slice(8,10));
+
+        AllirTonleikar.forEach(tonleikar => {
+            let tonleikaDagsetning = new Date(tonleikar.dateOfShow.slice(0,4), tonleikar.dateOfShow.slice(5,7), tonleikar.dateOfShow.slice(8,10));
+            if (tonleikaDagsetning <= fyrirDagsetninguDate) {
+                geraTonleika(tonleikar);
+            }
+        });
+    } else {
+        // Það er búið að hreinsa bæði hólfin svo að ég sýni allt
+        AllirTonleikar.forEach(tonleikar => {
+            geraTonleika(tonleikar);
+        });
     }
 }
 let eyðaÖllumBörnum = foreldri => {
@@ -100,6 +132,10 @@ let eyðaÖllumBörnum = foreldri => {
         foreldri.firstChild.remove();// Þetta eyðir fyrsta barninu
     }
 }
+
+// Hreinsa dagsetninga valið
+eftirDagsetningu.value = "";
+fyrirDagsetningu.value = "";
 
 // ----- Sækja JSON skránna -----
 fetch(nafnJSON)// Hérna er sótt JSON skránna
