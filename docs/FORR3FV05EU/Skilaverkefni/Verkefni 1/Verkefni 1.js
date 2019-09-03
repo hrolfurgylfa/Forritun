9d8aaa276c90e9489256a55332e9ae305d691305
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2019-09-03 15:11:15 +0000

    svg m. animation tilbúin fyrir utan comment

---
+++ docs/FORR3FV05EU/Skilaverkefni/Verkefni 1/Verkefni 1.js
@@ -0,0 +1,49 @@
+"use strict";
+
+// HTML Krækjur
+const svg_image = document.getElementById("emoji_svg");
+const emoji = document.getElementById("smily");
+const arrow_front = document.getElementById("arrow_front");
+const arrow_front_container = document.getElementById("arrow_front_container");
+const arrow_back = document.getElementById("arrow_back");
+
+// Föll
+let enableImageClick = () => {// Þetta fall keyrir eftir ákveðinn tíma og það bætir við onEventListener, þegar hann keyrir spilast animation þar sem fremri patrtur örvarinnar dettur af
+    svg_image.addEventListener("click", () => {// Þetta keyrir þegar það er ýtt á myndina og keyrir bara einu sinni
+        anime({// Þetta er animation þar sem fremri partur örvarinnar dettur af
+            targets: [arrow_front],// Þetta er það sem animationin hefur áhrif á
+            translateX: [
+                { value: "+=150", duration: 1500, delay: 0 },
+                { value: "+=0", duration: 1500, delay: 0 }
+            ],
+            translateY: [
+                { value: "-=100", duration: 1500, delay: 0 },
+                { value: "+=150", duration: 1500, delay: 0 }
+            ],
+            rotate: [
+                { value: "+=60", duration: 1500, delay: 0 }
+            ]
+        });
+    }, { once: true });// Þetta gerir það að verkum að þegar það er búið að keyra þennan event listiner getur hann ekki keyrt aftur
+}
+
+// Snúa emoji
+anime({
+    targets: [emoji],
+    rotate: 360,
+    duration: 800
+});
+
+// Skjóta ör
+anime({
+    targets: [arrow_front, arrow_back],
+    translateX: [-300, 0],
+    translateY: [-150, 10],
+    rotate: [30, 30],
+    duration: 300,
+    delay: 1000,
+    easing: 'easeInOutQuad'
+});
+
+// Passa að það sé ekki látið fremri part örvarinnar detta af of snemma
+window.setTimeout(enableImageClick, 1300);
