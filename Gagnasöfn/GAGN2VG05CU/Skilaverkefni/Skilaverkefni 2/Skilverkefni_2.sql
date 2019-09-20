9c52bbf5e2add932aba7302cf4ca02648473f841
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2019-09-20 11:54:19 +0000

    skilaverkefni 2 sótt

---
+++ Gagnasöfn/GAGN2VG05CU/Skilaverkefni/Skilaverkefni 2/Skilverkefni_2.sql
@@ -0,0 +1,31 @@
+/* 1:
+	Smíðið trigger fyrir insert into Restrictors skipunina. 
+	Triggernum er ætlað að koma í veg fyrir að einhver áfangi sé undanfari eða samfari síns sjálfs. 
+	með öðrum orðum séu courseNumber og restrictorID með sama innihald þá stoppar triggerinn þetta með
+	því að kasta villu og birta villuboð.
+	Dæmi um insert sem triggerinn á að stoppa: insert into Restrictors values('GSF2B3U','GSF2B3U',1);
+*/
+
+
+-- 2:
+-- Skrifið samskonar trigger fyrir update Restrictors skipunina.
+
+
+/*
+	3:
+	Skrifið stored procedure sem leggur saman allar einingar sem nemandinn hefur lokið.
+    Birta skal fullt nafn nemanda, heiti námsbrautar og fjölda lokinna eininga(
+	Aðeins skal velja staðinn áfanga. passed = true
+*/
+
+
+/*
+	4:
+	Skrifið 3 stored procedure-a:
+    AddStudent()
+    AddMandatoryCourses()
+    Hugmyndin er að þegar AddStudent hefur insertað í Students töfluna þá kallar hann á AddMandatoryCourses() sem skráir alla
+    skylduáfanga á nemandann.
+    Að endingu skrifið þið stored procedure-inn StudentRegistration() sem nota skal við sjálfstæða skráningu áfanga nemandans.
+*/
+
