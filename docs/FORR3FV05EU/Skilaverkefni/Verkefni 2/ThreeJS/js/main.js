4a847453ed00a0dce9cadab748734564fb48056d
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2019-09-27 11:59:56 +0000

    three.js byrjað

---
+++ docs/FORR3FV05EU/Skilaverkefni/Verkefni 2/ThreeJS/js/main.js
@@ -0,0 +1,33 @@
+// <-><-><-><-><-><-><-><-><-><-> Föll <-><-><-><-><-><-><-><-><-><->
+let animate = () => {
+    requestAnimationFrame( animate );
+    
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    
+	renderer.render( scene, camera );
+}
+
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+var loader = new THREE.GLTFLoader();
+loader.load('models/api.obj', gltf => {
+	scene.add( gltf.scene );
+}, undefined, error => {
+	console.error( error );
+});
+
+let renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+// Kassi
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+animate();
