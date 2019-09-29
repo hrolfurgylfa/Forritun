c20b3fa157028016fa76d09f781486676b36e41e
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2019-09-29 20:35:42 +0000

    api virkar og snýst

--- docs/FORR3FV05EU/Skilaverkefni/Verkefni 2/ThreeJS/js/main.js
+++ docs/FORR3FV05EU/Skilaverkefni/Verkefni 2/ThreeJS/js/main.js
@@ -1,37 +1,79 @@
-var scene = new THREE.Scene();
+// Ná í hleður textann til þess að sé hægt að eyða honum þegar það er búið að hlaða.
+const hledur_texti = document.getElementById("hledur_texti");
+// Ná í body sem verður sett onClickListiner á
+const body = document.getElementsByName("body")[0];
 
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+// Byrja senuna
+let scene = new THREE.Scene();
+
+// Setja upp myndavélina
+let camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 camera.position.z = 3;
 
-var renderer = new THREE.WebGLRenderer();
+// Setja upp rendererinn
+let renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var light = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
+let light = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
 light.position.set(-100, 0, 100);
-
-// var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
+light.name = "adal_ljos"
+// let fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
 // fillLight.position.set(100, 0, 100);
 
-// var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
+// let backLight = new THREE.DirectionalLight(0xffffff, 1.0);
 // backLight.position.set(100, 0, -100).normalize();
 
 scene.add(light);
 // scene.add(fillLight);
 // scene.add(backLight);
 
-var objLoader = new THREE.OBJLoader();
+let objLoader = new THREE.OBJLoader();
 objLoader.setPath('models/');
-objLoader.load('api.obj', object => {
+objLoader.load('api.obj', api => {
 
-    scene.add(object);
-    object.position.y = 0;
+    // Eyða hleður texta
+    hledur_texti.remove()
+
+    api.name = "api";
+    scene.add(api);
+    api.position.y = 0;
+
+    // Þetta er allt inni í þessu falli vegna þess að annars kom villa
+    // þegar ég reyndi að ná í þennan hlut í sceninu en núna get ég notað
+    // hann eðlilega
+
+    // Þessi breyta geymir hvenær síðasti rammi let renderaður og er notað til þess að reikna Delta Time.
+    let then = 0;
+    let snuningur = 0;
+
+    // Þetta teiknar senuna aftur og after, hvern einasta ramma
+    let render = now => {
+
+        now *= 0.001;  // Breyta í sekúndur
+        
+        // Reika Delta Time
+        const deltaTime = now - then;
+        then = now;
+        
+        // Þetta er snúnings hraðinn
+
+        // Hérna bæti ég delta time við snúninginn til þess að snúningurinn sé alltaf jafn haraður, sama hversu mikinn tíma tað tekur að teikna rammann. Ég deili deltatime með 1.5 til þess að apinn snúist hægar.
+        snuningur += deltaTime / 1.5;
+
+        // Þetta bætir snúningnum við apan
+        api.rotation.y = snuningur;
+
+        // Þetta teiknar senuna
+        renderer.render(scene, camera);
+
+        // Þetta segir vafranum að keyra fallið sem er sent inn í þetta fall þegar það er renderað næsta ramma.
+        requestAnimationFrame(render);
+    }
+    requestAnimationFrame(render);
 
 });
 
-var animate = () => {
-	requestAnimationFrame( animate );
-	renderer.render(scene, camera);
-};
-
-animate();
+body.addEventListener("click", evt => {
+    
+});
