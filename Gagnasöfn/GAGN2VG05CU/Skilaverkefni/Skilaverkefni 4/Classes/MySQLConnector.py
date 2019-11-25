4c820ca00a81cf6a6e1a4c9ef28e6a9a0f8efb9f
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2019-11-25 15:54:46 +0000

    MySQL connector tilbúinn

---
+++ Gagnasöfn/GAGN2VG05CU/Skilaverkefni/Skilaverkefni 4/Classes/MySQLConnector.py
@@ -0,0 +1,34 @@
+import pymysql.cursors
+
+class MySQLConnector():
+    def __init__(self, net_thjonn, notandi, adgangsord, gagnagrunnur):
+        try:
+            self.connection = pymysql.connect(
+                host=net_thjonn,
+                user=notandi,
+                password=adgangsord,
+                db=gagnagrunnur,
+                charset='utf8mb4',
+                cursorclass=pymysql.cursors.DictCursor,
+                autocommit=True
+            )
+        except RuntimeError:
+            self.__criticalError("Incorrect Password")
+            
+    def __criticalError(self, error_msg):
+        print("MySQL Connector critical error:",error_msg)
+        print("Shutting down")
+        exit()
+
+    def runSQL(self, sql):
+
+        try:
+            cursor = self.connection.cursor()
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            cursor.close()
+        except Exception as error:
+            print(error)
+            result = None
+
+        return result
