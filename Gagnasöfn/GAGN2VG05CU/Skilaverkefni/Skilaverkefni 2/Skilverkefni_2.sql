d0530bb4ec99986b0a5103dabe10fff08b6a21ba
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2019-11-29 21:02:51 +0000

    Verkefni 2 komið

--- Gagnasöfn/GAGN2VG05CU/Skilaverkefni/Skilaverkefni 2/Skilverkefni_2.sql
+++ Gagnasöfn/GAGN2VG05CU/Skilaverkefni/Skilaverkefni 2/Skilverkefni_2.sql
@@ -71,28 +71,95 @@ CALL KlaradarEiningar(4);
 DELIMITER $$
 DROP PROCEDURE IF EXISTS AddStudent $$
 
-CREATE PROCEDURE AddStudent(IN firstName_IN VARCHAR(55), lastName_IN VARCHAR(55), dob_IN DATE, startSemester_IN INT)
+CREATE PROCEDURE AddStudent(IN firstName_IN VARCHAR(55), lastName_IN VARCHAR(55), dob_IN DATE, BrautarID_IN INT)
 BEGIN
 	INSERT INTO Students
 		(firstName,lastName,dob,startSemester)
 	VALUES
-		(firstName_IN, lastName_IN, dob_IN, startSemester_IN);
+		(
+			firstName_IN,
+            lastName_IN,
+            dob_IN,
+            (SELECT CurrentSemester())
+		);
 	
-	CALL AddMandatoryCourses(LAST_INSERT_ID());
+	CALL AddMandatoryCourses(LAST_INSERT_ID(), BrautarID_IN);
 END $$
 DELIMITER ;
 
 DELIMITER $$
 DROP PROCEDURE IF EXISTS AddMandatoryCourses $$
 
-CREATE PROCEDURE AddMandatoryCourses(IN StudentID INT)
+CREATE PROCEDURE AddMandatoryCourses(IN NemendaID INT, BrautarID INT)
+begin
+	DECLARE i INT;
+    DECLARE n INT;
+	
+	SET i = 0;
+    
+	SELECT Count(trackID)
+	FROM TrackCourses 
+	WHERE mandatory = True AND Semester = 1 AND trackID = BrautarID
+    INTO n;
+
+	WHILE n > i
+	DO
+		INSERT INTO Registration
+			(studentID, trackID, courseNumber, registrationDate, passed, semesterID)
+		VALUES
+			(
+				NemendaID,
+				BrautarID, 
+				(-- Þetta Nær í course number af áfanganum sem er verið að lúppa í gegnum
+					SELECT courseNumber
+					FROM TrackCourses 
+					WHERE mandatory = True AND Semester = 1 AND trackID = BrautarID
+					LIMIT i, 1
+				),
+				CURRENT_DATE(),
+				False,
+				(SELECT CurrentSemester())
+			);
+        
+        SET i = i + 1;
+	END WHILE;
+end $$
+DELIMITER ;
+
+DELIMITER $$
+DROP FUNCTION IF EXISTS CurrentSemester $$
+    
+CREATE FUNCTION CurrentSemester()
+RETURNS INT
+NO SQL
 BEGIN
-	INSERT INTO Students
-		(firstName,lastName,dob,startSemester)
-	VALUES
-		(firstName_IN, lastName_IN, dob_IN, startSemester_IN);
+    RETURN(
+		SELECT semesterID
+		FROM Semesters
+		WHERE semesterStarts < CURRENT_DATE() AND semesterEnds > CURRENT_DATE()
+	);
+END $$
+DELIMITER ;
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS StudentRegistration $$
+
+CREATE PROCEDURE StudentRegistration(IN NemendaID INT, BrautarID INT, AfangaID CHAR(10), annarID INT)
+BEGIN
+	INSERT INTO Registration
+			(studentID, trackID, courseNumber, registrationDate, passed, semesterID)
+		VALUES
+			(
+				NemendaID,
+				BrautarID, 
+				AfangaID,
+                CURRENT_DATE(),
+                FALSE,
+                annarID
+			);
 END $$
 DELIMITER ;
 
 
-CALL AddStudent("Kalli", "Klikkaði", "2019-11-21", 9);
+CALL AddStudent("Jón", "Spæjó", "1642-4-1", 9);
+CALL StudentRegistration(3, 9, "GSF2B3U", CurrentSemester());
